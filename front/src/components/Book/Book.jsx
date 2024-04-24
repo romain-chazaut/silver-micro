a423fc0ff882a0174dd3aa71704136ee7df11be3
Maëlle Lagarde <maelle.lagarde@seetickets.fr>
2024-04-24 16:31:03 +0200

    display date +30min

--- front/src/components/Book/Book.jsx
+++ front/src/components/Book/Book.jsx
@@ -9,8 +9,14 @@ export default function Book() {
     currentDate.setHours(12);
     currentDate.setMinutes(0);
 
-    const formattedDate = `${currentDate.getDate()}-${currentDate.getMonth() + 1}-${currentDate.getFullYear()} ${currentDate.getHours()} h`;
+    // const formattedDate = `${currentDate.getDate()}-${currentDate.getMonth() + 1}-${currentDate.getFullYear()} ${currentDate.getHours()} h`;
+    
+    const newDate = new Date(currentDate); // Créer une nouvelle instance de date
+    console.log(newDate);
+    newDate.setMinutes(newDate.getMinutes() + 30);
 
+    console.log(newDate);
+    
     // Au fetch on recois data
     // new date(data.dateReservation)
 
@@ -19,17 +25,24 @@ export default function Book() {
     
     function DateRender() {
         let agenda = [];
-        const displayDate = currentDate.toLocaleString();
+        // const displayDate = currentDate.toLocaleString(); // 12h 
+        let date1 = new Date(currentDate);
+        
         for(let i = 0; i < placesAvailable; i++) {
+            date1.setMinutes(date1.getMinutes() + 30);
+            const formattedDate = `${date1.getDate()}-${date1.getMonth() + 1}-${date1.getFullYear()} ${date1.getHours()} h-${date1.getMinutes()}`;
+            console.log(formattedDate,'coucoiu');
+            // si ici je fais 12h + 30 min 
             agenda.push(formattedDate);
+
         }
     
-        const list = agenda.map(date =>
-            <button>{date}</button>
+        const list = agenda.map((date,index) =>
+            <button id={`plop-${index}`}>{date}</button>
         );
 
         return(
-            <div>
+            <div id="tonpere">
                 {list}
             </div>
         )
